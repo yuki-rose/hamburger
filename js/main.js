c8be28de10eb56aafe870207066ea483724a63bc
yuki-rose <154495079+yuki-rose@users.noreply.github.com>
2024-02-03 01:11:01 +0900

    スマホサイト修正

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ jQuery( function($) {
         $(".l-side").toggleClass("is-open");
     });
 });
+
+$(window).resize(function( ){
+    let h = 0;
+    $('.l-main__wrapperGrid-txtboxset').each(function() {
+        if($(this).height() > h ){
+            h = $(this).height();
+        }
+    });
+    let ItemHeight = h +314
+    $('.l-main__wrapperGrid-item').height(ItemHeight);
+});
+
+$(window).resize(function() {
+    let mh = $('.l-main__map-txtset').height();
+let imgh = mh + 80
+$('.l-main__map-img').height(imgh);
+});
