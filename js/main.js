1e0113d454d821a0e0fa57e075f9c1f9adb6c9ff
yuki-rose <154495079+yuki-rose@users.noreply.github.com>
2024-03-07 19:23:04 +0900

    メニュー背景固定

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ jQuery( function($) {
     $(".js-menu__btn").on("click", function() {
         $(".l-side__base").addClass("is-open");
         $(".l-side__bg").addClass("is-open");
+        $("body").css({overflow: "hidden" });
     });
 });
 
@@ -9,6 +10,7 @@ jQuery( function($) {
     $(".p-menu__closebtn").on("click", function() {
         $(".l-side__base").removeClass("is-open");
         $(".l-side__bg").removeClass("is-open");
+        $("body").css({overflow: "auto" });
     });
 });
 
@@ -24,15 +26,16 @@ $(window).on("load resize", function() {
     console.log(vw);
     let bp = 960;
     if (vw <= bp) {
-        let headerh = $('.l-header').height();
-        console.log(headerh);
-        let mainh = $('.l-main').outerHeight(true);
-        console.log(mainh);
-        let footerh = $('.l-footer').outerHeight(true);
-        console.log(footerh);
-        sh = parseInt(headerh) + parseInt(mainh) + parseInt(footerh);
-        console.log(sh);
-        $('.l-side__base').height(sh);
+        //let headerh = $('.l-header').height();
+        //console.log(headerh);
+        //let mainh = $('.l-main').outerHeight(true);
+        //console.log(mainh);
+        //let footerh = $('.l-footer').outerHeight(true);
+        //console.log(footerh);
+        //sh = parseInt(headerh) + parseInt(mainh) + parseInt(footerh);
+        //console.log(sh);
+        //$('.l-side__base').height(sh);
+        ;
     } else {
         let vw = $(window).width();
         console.log(vw);
