c11fa988033d629645351b2709e9800202dde2b7
yuki-rose <154495079+yuki-rose@users.noreply.github.com>
2024-03-31 02:52:49 +0900

    メニューjs修正

--- js/main.js
+++ js/main.js
@@ -1,40 +1,31 @@
 jQuery( function($) {
-    $(".js-menu__btn").on("click", function() {
+    $(document).on("click",".js-menu__btn", function() {
         $(".l-side__base").addClass("is-open");
         $(".l-side__bg").addClass("is-open");
-        $("body").css({overflow: "hidden" });
+        $("body").addClass("is-open");
     });
 });
 
 jQuery( function($) {
-    $(".p-menu__closebtn").on("click", function() {
+    $(document).on("click",".p-menu__closebtn", function() {
         $(".l-side__base").removeClass("is-open");
         $(".l-side__bg").removeClass("is-open");
-        $("body").css({overflow: "auto" });
+        $("body").removeClass("is-open");
     });
 });
 
 jQuery( function($) {
-    $(".l-side__bg").on("click", function() {
+    $(document).on("click",".l-side__bg", function() {
         $(".l-side__base").removeClass("is-open");
         $(".l-side__bg").removeClass("is-open");
     });
 });
 
-$(window).on("load resize", function() { 
+$(document).on("load resize",window, function() { 
     let vw = $(window).width();
     console.log(vw);
     let bp = 960;
     if (vw <= bp) {
-        //let headerh = $('.l-header').height();
-        //console.log(headerh);
-        //let mainh = $('.l-main').outerHeight(true);
-        //console.log(mainh);
-        //let footerh = $('.l-footer').outerHeight(true);
-        //console.log(footerh);
-        //sh = parseInt(headerh) + parseInt(mainh) + parseInt(footerh);
-        //console.log(sh);
-        //$('.l-side__base').height(sh);
         ;
     } else {
         let vw = $(window).width();
